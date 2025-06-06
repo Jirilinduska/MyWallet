d66982e22b4f40ba1b72c6db611af45c8d1d5657
Jiří Linduška <jiric.linduska@seznam.cz>
2025-06-06 23:05:01 +0200

    Přidání ikonek pro kategorie

--- client/src/utils/icons/category-icons.tsx
+++ client/src/utils/icons/category-icons.tsx
@@ -1,31 +1,69 @@
-import { IconBasket, IconBitcoin, IconCar, IconCarDashboard, IconCigarette, IconCoffee, IconCut, IconDanger, IconDollar, IconDrink, IconEdit, IconEyeShow, IconGameController, IconGift, IconHiking, IconHome, IconLaptop, IconLightbulb, IconLove, IconPainter, IconPill, IconPizza, IconSalary, IconShield, IconTshirt, IconWallet } from "./icons"
+import {
+  IconBasket,
+  IconBitcoin,
+  IconBook,
+  IconCar,
+  IconCarDashboard,
+  IconCart,
+  IconCigarette,
+  IconCoffee,
+  IconCut,
+  IconDanger,
+  IconDollar,
+  IconDrink,
+  IconEdit,
+  IconEyeShow,
+  IconFitness,
+  IconGameController,
+  IconGift,
+  IconHiking,
+  IconHome,
+  IconLaptop,
+  IconLightbulb,
+  IconLove,
+  IconPainter,
+  IconPaw,
+  IconPill,
+  IconPizza,
+  IconSalary,
+  IconShield,
+  IconTools,
+  IconTshirt,
+  IconWallet,
+  IconWeight,
+} from "./icons";
 
 export const categoryIcons = [
-
-    { id: 1, iconJSX: <IconPizza/> }, // FOOD
-    { id: 2, iconJSX: <IconCar/> }, // TRANSPORT
-    { id: 3, iconJSX: <IconDrink/> }, // ENTERTAINMENT
-    { id: 4, iconJSX: <IconSalary/> }, // SALARY
-    { id: 5, iconJSX: <IconLaptop/> }, // FREELANCE
-    { id: 6, iconJSX: <IconLightbulb/> }, // OTHER
-    { id: 7, iconJSX: <IconCarDashboard/> }, 
-    { id: 8, iconJSX: <IconPainter/> }, 
-    { id: 9, iconJSX: <IconGift/> }, 
-    { id: 10, iconJSX: <IconHome/> }, 
-    { id: 11, iconJSX: <IconLove/> }, 
-    { id: 12, iconJSX: <IconCoffee/> }, 
-    { id: 13, iconJSX: <IconBitcoin/> }, 
-    { id: 14, iconJSX: <IconDanger/> }, 
-    { id: 15, iconJSX: <IconBasket/> }, 
-    { id: 16, iconJSX: <IconEdit/> }, 
-    { id: 17, iconJSX: <IconEyeShow/> }, 
-    { id: 18, iconJSX: <IconCigarette/> }, 
-    { id: 19, iconJSX: <IconGameController/> }, 
-    { id: 20, iconJSX: <IconTshirt/> }, 
-    { id: 21, iconJSX: <IconCut/> }, 
-    { id: 22, iconJSX: <IconPill/> }, 
-    { id: 23, iconJSX: <IconShield/> }, 
-    { id: 24, iconJSX: <IconHiking/> }, 
-    { id: 25, iconJSX: <IconDollar/> }, 
-    { id: 26, iconJSX: <IconWallet/> }, 
-]
+  { id: 1, iconJSX: <IconPizza /> }, // FOOD
+  { id: 2, iconJSX: <IconCar /> }, // TRANSPORT
+  { id: 3, iconJSX: <IconDrink /> }, // ENTERTAINMENT
+  { id: 4, iconJSX: <IconSalary /> }, // SALARY
+  { id: 5, iconJSX: <IconLaptop /> }, // FREELANCE
+  { id: 6, iconJSX: <IconLightbulb /> }, // OTHER
+  { id: 7, iconJSX: <IconCarDashboard /> },
+  { id: 8, iconJSX: <IconPainter /> },
+  { id: 9, iconJSX: <IconGift /> },
+  { id: 10, iconJSX: <IconHome /> },
+  { id: 11, iconJSX: <IconLove /> },
+  { id: 12, iconJSX: <IconCoffee /> },
+  { id: 13, iconJSX: <IconBitcoin /> },
+  { id: 14, iconJSX: <IconDanger /> },
+  { id: 15, iconJSX: <IconBasket /> },
+  { id: 16, iconJSX: <IconEdit /> },
+  { id: 17, iconJSX: <IconEyeShow /> },
+  { id: 18, iconJSX: <IconCigarette /> },
+  { id: 19, iconJSX: <IconGameController /> },
+  { id: 20, iconJSX: <IconTshirt /> },
+  { id: 21, iconJSX: <IconCut /> },
+  { id: 22, iconJSX: <IconPill /> },
+  { id: 23, iconJSX: <IconShield /> },
+  { id: 24, iconJSX: <IconHiking /> },
+  { id: 25, iconJSX: <IconDollar /> },
+  { id: 26, iconJSX: <IconWallet /> },
+  { id: 27, iconJSX: <IconFitness /> },
+  { id: 28, iconJSX: <IconCart /> },
+  { id: 29, iconJSX: <IconTools /> },
+  { id: 30, iconJSX: <IconPaw /> },
+  { id: 31, iconJSX: <IconBook /> },
+  { id: 32, iconJSX: <IconWeight /> },
+];
--- client/src/utils/icons/icons.tsx
+++ client/src/utils/icons/icons.tsx
@@ -1,9 +1,9 @@
 import { IoMdEye, IoIosEyeOff, IoMdAddCircle, IoIosLaptop, IoLogoGameControllerB, IoMdSettings, IoMdArrowBack, IoIosNotifications } from "react-icons/io"
 import { IoMail, IoCard, IoHome, IoClose, IoCheckmarkSharp, IoChevronDown, IoCloseCircle } from "react-icons/io5"
-import { MdDashboard, MdCategory, MdDeleteForever, MdOutlineSmokingRooms, MdAttachMoney, MdOutlineSort, MdAdminPanelSettings, MdMarkEmailRead } from "react-icons/md";
-import { GiMoneyStack, GiTakeMyMoney, GiPill,  } from "react-icons/gi";
+import { MdDashboard, MdCategory, MdDeleteForever, MdOutlineSmokingRooms, MdAttachMoney, MdOutlineSort, MdAdminPanelSettings, MdMarkEmailRead, MdFitnessCenter } from "react-icons/md";
+import { GiMoneyStack, GiTakeMyMoney, GiPill, GiWeightLiftingUp } from "react-icons/gi";
 import { RiMenu3Line } from "react-icons/ri";
-import { FaUserTie, FaSort, FaCar , FaMoneyBillWave, FaPiggyBank, FaTshirt, FaShieldAlt, FaHiking, FaWallet, FaCheckCircle, FaStar, FaFileArchive, FaRegFileArchive, FaChevronDown } from "react-icons/fa";
+import { FaUserTie, FaSort, FaCar , FaMoneyBillWave, FaPiggyBank, FaTshirt, FaShieldAlt, FaBook, FaHiking, FaWallet, FaCheckCircle, FaStar, FaFileArchive, FaRegFileArchive, FaChevronDown, FaShoppingCart, FaTools, FaPaw } from "react-icons/fa";
 import { FaPowerOff, FaRegLightbulb, FaChartLine, FaRegCalendarMinus, FaRegCalendarPlus, FaCircleInfo, FaCircleExclamation } from "react-icons/fa6";
 import { GrFormPrevious, GrFormNext, GrStorage } from "react-icons/gr";
 import { CiPizza, CiEdit } from "react-icons/ci";
@@ -80,5 +80,11 @@ export {
     GrStorage as IconDB,
     FaFileArchive as IconArchived,
     FaRegFileArchive as IconArchive,
-    FaChevronDown as IconArrowDown
+    FaChevronDown as IconArrowDown,
+    MdFitnessCenter as IconFitness,
+    FaShoppingCart as IconCart,
+    FaTools as IconTools,
+    FaPaw as IconPaw,
+    FaBook as IconBook,
+    GiWeightLiftingUp as IconWeight
 }
